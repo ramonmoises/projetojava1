52e64e3ba2d363c7e9fcd13a11a07ee5f8d9589f
ramon <ramonmoises>
2022-01-04 00:18:07 -0300

    Criação do projeto direto pelo eclipse

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em java no eclipse do curso - Formação Java Web.");
+		System.out.println("Java é orientada a objetos.");
+	}
+
+}
